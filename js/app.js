29eddb6c8086258ce5fc444f9fdb41773d0f30d3
Alberto Caminos <albeerc@gmail.com>
2022-07-03 18:51:30 -0300

    03/07

---
+++ js/app.js
@@ -0,0 +1,14 @@
+/*
+let bioBtn = document.querySelector("#btn");
+bioBtn.addEventListener("click",mostrar);
+
+function mostrar (){
+    let info = document.querySelector("#bio-info");
+    info.innerHTML = 'Tiago Uriel Pacheco Lezcano, alias Tiago PZK es un artista Argentino de 20 años de edad que desde sus inicios crece a pasos agigantados. Es reconocido por su versatilidad a la hora de componer y crear música y sus canciones que van desde el trap hasta el R&B hacen que sea difícil etiquetarlo en un solo género. Fresco, transparente y decidido Tiago ha competido en las más conocidas batallas de freestyle como God Level, Nike Battle Force, Supremacía, GoldBattle, El Quinto Escalón y Red Bull “Batalla de los Gallos”.<button id="cerrar">x</button>';
+    let cerrar = document.querySelector("#cerrar");
+    cerrar.addEventListener("click",function(){
+        info.innerHTML = '';
+    });
+}
+
+*/
