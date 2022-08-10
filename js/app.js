994104a4e2a0c459bfec831c96b68e189ce8f88c
Alberto Caminos <albeerc@gmail.com>
2022-08-10 19:45:45 -0300

    Biografía con JS

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
-/*
-let bioBtn = document.querySelector("#btn");
-bioBtn.addEventListener("click",mostrar);
+const bioBtn = document.querySelector(".footer__boton");
+const info = document.querySelector(".bioInfo");
+const btnCerrar = document.querySelector(".btnCerrar")
 
-function mostrar (){
-    let info = document.querySelector("#bio-info");
-    info.innerHTML = 'Tiago Uriel Pacheco Lezcano, alias Tiago PZK es un artista Argentino de 20 años de edad que desde sus inicios crece a pasos agigantados. Es reconocido por su versatilidad a la hora de componer y crear música y sus canciones que van desde el trap hasta el R&B hacen que sea difícil etiquetarlo en un solo género. Fresco, transparente y decidido Tiago ha competido en las más conocidas batallas de freestyle como God Level, Nike Battle Force, Supremacía, GoldBattle, El Quinto Escalón y Red Bull “Batalla de los Gallos”.<button id="cerrar">x</button>';
-    let cerrar = document.querySelector("#cerrar");
-    cerrar.addEventListener("click",function(){
-        info.innerHTML = '';
-    });
-}
+// Mostrar mensaje cuando se presiona el botón de bio
+bioBtn.addEventListener("click", function() {
+    info.classList.add("display");
+});
 
-*/
+// Cerrar el mensaje cuando se presiona el botón de X
+btnCerrar.addEventListener("click", function() {
+    info.classList.remove("display")
+});
